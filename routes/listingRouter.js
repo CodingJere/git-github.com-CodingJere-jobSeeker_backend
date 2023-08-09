68292bee946195eb74e60738736b0f2dc063fd2b
Engineering <jeremy_engineering@Jeremys-Laptop.local>
2023-08-09 15:58:09 +0300

    spacing formatting

--- routes/listingRouter.js
+++ routes/listingRouter.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const listingsController = require("../controller/listingsController");
 //get listing
 router.get("/:id", listingsController.getListing);
-
 //get listings
 router.get("/", listingsController.getListings);
-
 //post listing
 router.post("/", listingsController.postListing);
 
